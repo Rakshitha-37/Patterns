fca5a9e8ae839251a8949ebbf3c67ee3cc34d735
Rakshitha-37 <96290375+Rakshitha-37@users.noreply.github.com>
2022-07-21 19:28:24 +0530

    Update pattern11.c

--- pattern11.c
+++ pattern11.c
@@ -17,57 +17,6 @@ int main()
     {
         for(int col = 0;col <= row;col++)
         {
-            if((row % 2) == 0)
-            {
-                printf("%c ", 97);
-            }
-            else
-            {
-                printf("%c ", 65);
-            }
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
-#include <stdio.h>
-
-int main()
-{
-    int num;
-    printf("Enter number: ");
-    scanf("%d", &num);
-    for(int row = 0;row < num;row++)
-    {
-        for(int col = 0;col <= row;col++)
-        {
-            if((row % 2) == 0)
-            {
-                printf("%c ", 97);
-            }
-            else
-            {
-                printf("%c ", 65);
-            }
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
-
-#include <stdio.h>
-
-int main()
-{
-    int num;
-    printf("Enter number: ");
-    scanf("%d", &num);
-    for(int row = 0;row < num;row++)
-    {
-        for(int col = 0;col <= row;col++)
-        {
             printf("%d ", row-col+1);
         }
         printf("\n");
